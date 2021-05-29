87cc0ec0561194b708da7b0ddb3833c1d52148f4
KieuLoc <kieuxuanloc121200@gmail.com>
2021-05-29 14:50:18 +0700

    second demo git

---
+++ main.js
@@ -0,0 +1,13 @@
+var hover = document.getElementById('heading')
+console.log({
+    element: hover
+})
+var test = document.querySelector('h1')
+test.className = 'tests'
+
+var chek = document.querySelector('.box')
+chek.innerHTML = '<h1 title="Heading">faaa</h1>'
+
+console.log(chek.innerHTML)
+
+console.log(document.querySelector('h1').innerText)
